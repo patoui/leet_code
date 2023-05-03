72e9011fe171eb49499e181b543c9d0d182d20d5
Patrique Ouimet <patrique.ouimet@gmail.com>
2023-05-03 07:52:34 -0400

    chore(longest_common_prefix): revised

--- 14_longest_common_prefix/main.go
+++ 14_longest_common_prefix/main.go
@@ -3,25 +3,14 @@ package main
 import "fmt"
 
 func longestCommonPrefix(strs []string) string {
-	var longestPrefix string
-	longestPrefix, strs = strs[0], strs[1:]
+	longestPrefix := strs[0]
 
 	for _, v := range strs {
-		strToLoop := longestPrefix
-		strToCompare := v
-		if (len(strToCompare) < len(strToLoop)) {
-			strToLoop, strToCompare = strToCompare, strToLoop
-		}
-
-		prefixChars := ""
-		for i, c := range []byte(strToLoop) {
-			if (c != strToCompare[i]) {
-				break;
-			}
-
-			prefixChars = prefixChars + string(c)
-		}
-		longestPrefix = prefixChars
+		i := 0
+	
+		for ; i < len(v) && i < len(longestPrefix) && v[i] == longestPrefix[i]; i++ {}
+	
+		longestPrefix = longestPrefix[:i]
 	}
 
 	return longestPrefix
