0378a34aaf6ffc231aac2af305e27a1fc0679d8a
Patrique Ouimet <patrique.ouimet@gmail.com>
2023-05-08 07:59:31 -0400

    chore: completed valid parantheses

---
+++ 20_valid_parantheses/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func isValid(s string) bool {
+	var cop rune
+	var op []rune
+
+	for _, v := range s {
+		if v == '[' || v == '(' || v == '{' {
+			op = append(op, v)
+		} else {
+			cop, op = op[len(op)-1], op[:len(op)-1]
+
+			if v == ']' && cop != '[' {
+				return false;
+			} else if v == ')' && cop != '(' {
+				return false;
+			} else if v == '}' && cop != '{' {
+				return false;
+			}
+		}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println("EXAMPLE 1")
+	str1 := "()"
+	result1 := isValid(str1)
+	fmt.Printf("%t\n", result1)
+
+	fmt.Println("EXAMPLE 2")
+	str2 := "()[]{}"
+	result2 := isValid(str2)
+	fmt.Printf("%t\n", result2)
+
+	fmt.Println("EXAMPLE 3")
+	str3 := "(]"
+	result3 := isValid(str3)
+	fmt.Printf("%t\n", result3)
+}
+
